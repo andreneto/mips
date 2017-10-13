53f02c81749fbed82e3c2c737d2e0feb5db8abcb
Marina Barros <mob@cin.ufpe.br>
2017-10-13 11:17:15 -0300

    Fix SignExtend16to32

--- components/SignExtend16to32.v
+++ components/SignExtend16to32.v
@@ -1,6 +1,6 @@
 module SignExtend16to32(in, out);
 	input wire [15:0] in;
-	output reg [31:0] out;
+	output wire [31:0] out;
 	
-	assign out = in >> 16;
+	assign out = (in[15] == 1'b1) ? {{16{1'b1}}, {in}} : {{16{1'b0}}, {in}};
 endmodule
