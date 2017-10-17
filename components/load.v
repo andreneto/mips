9a3fc07011545ed442c372ca01116aec9f1f05b1
André Neto <jacn@cin.ufpe.br>
2017-10-17 19:16:21 -0300

    Fix minor bugs in load module

--- components/load.v
+++ components/load.v
@@ -1,9 +1,9 @@
 module load(mem_data, load_type, out_data);
 
   input wire [1:0] load_type;
-  input reg [31:0] mem_data;
-  output reg [31:0] out_data;
+  input wire [31:0] mem_data;
+  output wire [31:0] out_data;
 
-  assign out_data = (load_type == 2'b00)? {mem_data[31:24], 24'b0} : ((load_type == 2'b01)? {mem_data[31:16], 16'b0} : ((load_type == 2'b10)? mem_data : ((load_type == 2'b11)? 32'b0 : 32'b0))));
+  assign out_data = (load_type == 2'b00) ? { mem_data[31:24], 24'b0} : ((load_type == 2'b01)? {mem_data[31:16], 16'b0} : ((load_type == 2'b10)? mem_data : ((load_type == 2'b11)? 32'b0 : 32'b0)));
 
 endmodule
