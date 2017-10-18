bacd137519a77b1225bfb37bbc446de8b14bd5c3
André Neto <jacn@cin.ufpe.br>
2017-10-17 22:51:01 -0300

    Update mux2 input reference

--- components/mux2.v
+++ components/mux2.v
@@ -1,12 +1,12 @@
-module mux2(in_1, in_2, control, out);
-	input wire [31:0] in_1, in_2;
+module mux2(in_0, in_1, control, out);
+	input wire [31:0] in_0, in_1;
 	input wire control;
 	output reg [31:0] out;
 	
 	always@(control) begin
 		case(control)
-			0: out = in_1;
-			1: out = in_2;
+			0: out = in_0;
+			1: out = in_1;
 		endcase
 	end
 endmodule	
