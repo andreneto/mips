8bdcdc6ebd4d485bb84272b4ec5bf155dcf1c0d6
André Neto <jacn@cin.ufpe.br>
2017-10-27 00:30:54 -0300

    Fix Load module

--- components/Load.v
+++ components/Load.v
@@ -4,6 +4,6 @@ module Load(mem_data, load_type, out_data);
   input wire [31:0] mem_data;
   output wire [31:0] out_data;
 
-  assign out_data = (load_type == 2'b00) ? { mem_data[31:24], 24'b0} : ((load_type == 2'b01)? {mem_data[31:16], 16'b0} : ((load_type == 2'b10)? mem_data : ((load_type == 2'b11)? 32'b0 : 32'b0)));
+  assign out_data = (load_type == 2'b00) ? { {24{mem_data[7]}} , mem_data[7:0] } : ((load_type == 2'b01)? { {16{mem_data[15]}} , mem_data[15:0] } : ((load_type == 2'b10)? mem_data : ((load_type == 2'b11)? 32'b0 : 32'b0)));
 
 endmodule
